2fae842e03ee6d17c5a8cf56ceed64bf3abc1cf7
mava <mavafiza@gmail.com>
2023-11-09 05:31:39 +0100

    answer, contact, index, styles, trips

--- index.js
+++ index.js
@@ -6,3 +6,17 @@ function myFunction() {
         x.className = "nav";
     }
 }
+
+function mostrarFrase() {
+    let frase = "¡Felicidades! Has reservado tu viaje galáctico. ¡Prepárate para la aventura!";
+    // Redireccionar a la página ./answer.html con la frase como parámetro
+    window.location.href = "./answer.html?frase=" + encodeURIComponent(frase);
+}
+
+function obtenerParametro(nombre) {
+    const urlParams = new URLSearchParams(window.location.search);
+    return urlParams.get(nombre);
+}
+
+// Mostrar la frase en el párrafo
+document.getElementById("fraseDiv").innerText = obtenerParametro("frase");
